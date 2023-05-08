0d6db56917e1032c16838c4ca2f2fcf0ac068475
david domotor <dd19900618@gmail.com>
2023-05-08 17:02:14 +0200

    .

--- infra/cluster.tf
+++ infra/cluster.tf
@@ -17,7 +17,12 @@ resource "aws_route_table" "my_route_table_mern" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.my_igw_mern.id
   }
-  subnet_ids = [aws_subnet.eks_subnet_cluster_mern.id]
+  
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.eks_subnet_cluster_mern.id
+  route_table_id = aws_route_table.my_route_table_mern.id
 }
 
 
@@ -26,6 +31,8 @@ resource "aws_subnet" "eks_subnet_cluster_mern" {
   vpc_id            = aws_vpc.eks_vpc_cluster_mern.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "eu-central-1a"
+  map_public_ip_on_launch = true
+
 }
 
 
